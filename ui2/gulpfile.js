3877d3f45c9a7d18b9f63a8719edaa6f89a37e70
sun202x <sun202x@gmail.com>
2021-10-24 17:53:45 +0900

    ui2

---
+++ ui2/gulpfile.js
@@ -0,0 +1,53 @@
+const webserver = require('gulp-webserver'),
+    typescript = require('rollup-plugin-typescript2'),
+    rollup = require('rollup'),
+    resolve = require('@rollup/plugin-node-resolve'),
+    commonjs = require('@rollup/plugin-commonjs'),
+    injectProcessEnv = require('rollup-plugin-inject-process-env'),
+    gulp = require('gulp');
+
+gulp.task('build', () => {
+    return rollup
+        .rollup({
+            input: './src/index.tsx',
+            external: ['react'],
+            plugins: [
+                resolve.nodeResolve(),
+                commonjs(),
+                typescript(),
+                injectProcessEnv({
+                    NODE_ENV: 'production'
+                }),
+            ]
+        })
+        .then(bundle => {
+            return bundle.write({
+                file: './dist/index.js',
+                format: 'umd',
+                name: 'Main',
+                sourcemap: true,
+                globals: {
+                    'react': 'React',
+                    'react-dom': 'ReactDOM'
+                }
+            });
+        });
+});
+
+gulp.task('copy-html', function() {
+    return gulp.src('./public/index.html')
+        .pipe(gulp.dest('dist'));
+});
+
+// gulp 서버
+gulp.task('webserver', function () {
+    gulp.src('./dist')
+        .pipe(webserver({
+            fallback: "index.html",
+            livereload: true,
+            open: true,
+            port: 8080
+        }));
+});
+
+gulp.task('default', gulp.series(['build', 'copy-html', 'webserver']));
---
+++ ui2/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      Hello, gulp and typescript!!
+    </div>
+  );
+}
+
+export default App;
